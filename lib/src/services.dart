3c41bd19805477bcfe21a2e4bd73309557dab1c4
Rithvik Nishad <rithvikn2001@gmail.com>
2020-11-26 09:43:13 +0530

    added Hardwares to Services

--- lib/src/services.dart
+++ lib/src/services.dart
@@ -1,6 +1,7 @@
 import 'package:bsi_dart/bsi_dart.dart';
 
 abstract class Services {
+  /// Private generative constructor to prevent instantiation of this class.
   Services._();
 
   /// The root [Service] of the **BakeCode Ecosystem**.
@@ -8,6 +9,9 @@ abstract class Services {
   /// It is strongly recommended that all services runs as a child or sub-child
   /// of this root service.
   static final BakeCode = ServiceReference.root('bakecode');
+
+  /// The hardware service, where all hardware types can be instantiated to.
+  static final Hardwares = BakeCode.child('hardwares');
 
   /// The **Broadcast [Service]** for every BSI applications.
   ///
