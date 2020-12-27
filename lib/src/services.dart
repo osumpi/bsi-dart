1e08fd12b707334fbf17c98380e34c8db288bef3
Rithvik Nishad <rithvikn2001@gmail.com>
2020-12-27 19:59:23 +0530

    rn EndPointDevices to Devices

--- lib/src/services.dart
+++ lib/src/services.dart
@@ -11,7 +11,7 @@ abstract class Services {
   static final BakeCode = ServiceReference.root('bakecode');
 
   /// The end point devices [Service] where all devices are accessible.
-  static final EndPointDevices = ServiceReference.root('dev');
+  static final Devices = ServiceReference.root('dev');
 
   /// The hardware service, where all hardware types can be instantiated to.
   static final Hardwares = BakeCode.child('hardwares');
